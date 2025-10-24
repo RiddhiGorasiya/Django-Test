73686850eead49368cb07a54a595b7519669e24c
gorasiyariddhi4@gmail.com <gorasiyariddhi4@gmail.com>
2025-10-24 18:06:48 +0530

    add range and filters

--- CRUD-FBV/crud/myapp/views.py
+++ CRUD-FBV/crud/myapp/views.py
@@ -3,6 +3,7 @@ from .forms import StudentRegistration
 from .models import User
 from django.contrib import messages
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 # Create your views here.
 
@@ -20,10 +21,29 @@ def add_show(request):
             messages.success(request, 'New Student Successfully Saved !')
             fm = StudentRegistration()
     else:
-        fm = StudentRegistration()
+        fm = StudentRegistration()              
+    search_query = request.GET.get('search', '').strip()
+    sort_by = request.GET.get('sort', 'id')
+    order = request.GET.get('order', 'asc')
+    id_min = request.GET.get('id_min')
+    id_max = request.GET.get('id_max')
     stud = User.objects.all()
+    # Keyword Filter
+    if search_query:
+        stud = stud.filter(Q(name__icontains=search_query) | Q(email__icontains=search_query))
+    # Range Filter
+    if id_min and id_max:
+        stud = stud.filter(id__range=(id_min, id_max))
+    elif id_min:
+        stud = stud.filter(id__gte=id_min)
+    elif id_max:
+        stud = stud.filter(id__lte=id_max)
+    # Sorting
+    if order == 'desc':
+        sort_by = f'-{sort_by}'
+    stud = stud.order_by(sort_by)
     # add a pagination 
-    all_post = User.objects.all().order_by('-id')
+    all_post = User.objects.all().order_by('id')
     paginator = Paginator(all_post, per_page = 4) # Show 5 contacts per page.
     page_number = request.GET.get('page')
     stud = paginator.get_page(page_number)
